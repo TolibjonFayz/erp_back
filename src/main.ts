a4984bc4ec8298bf634ce5f1ecde7c548ad7666c
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2023-11-07 18:11:18 +0500

    Global prefix 'api' is working

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,25 @@ import { ValidationPipe } from '@nestjs/common';
 
 const start = async () => {
   try {
+    const PORT = process.env.PORT || 3300;
+
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+
     const config = new DocumentBuilder()
       .setTitle('ERP')
       .setDescription('A project named ERP by Tolibjon')
       .setVersion('1.0.0')
       .addTag('NestJS, Postgres, Sequelize, Vuejs, TailwindCss')
       .build();
-
-    const PORT = process.env.PORT || 3300;
-    const app = await NestFactory.create(AppModule);
-
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/api/docs', app, document);
 
     app.use(cookieParser());
-    app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
+
     await app.listen(PORT, () => {
-      console.log(`Server is running at ${PORT}`);
+      console.log(`Server is listening on port ${PORT}`);
     });
   } catch (error) {
     console.log(error);
